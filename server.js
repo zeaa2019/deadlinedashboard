9cfe03aa5a9751b9aa2ecb22fe61f897c20bee6d
Zainab <zainab.abdulsalam2014@gmail.com>
2019-04-03 18:59:34 +0100

    form created

--- server.js
+++ server.js
@@ -2,16 +2,14 @@
 
 const express = require('express');
 const app = express();
+
 const router = express.Router();
 
 
 app.use(express.static('webpages'));
 
-router.get('/admin', postDeadline);
 
-function postDeadline() {
-
-  console.log("intnrfn");
-}
 
 app.listen(process.env.PORT || 8080);
+
+console.log("localhost:8080");
